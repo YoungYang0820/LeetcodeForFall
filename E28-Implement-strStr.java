df326c214138c3c15fcf22ba0462b3d17201a6f0
young <youngyang0820@gmail.com>
2018-06-02 06:36:35 +0800

    2018.6.1 add 25-29

---
+++ E28-Implement-strStr.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            int j = 0;
+            for (; j < needle.length(); j++) {
+                if (i+j >= haystack.length() || haystack.charAt(i+j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == needle.length()) return i;
+        }
+        return -1;
+    }
+}
